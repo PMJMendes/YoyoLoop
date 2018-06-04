de247fa78039514fbec35678c49dd4f9979dafda
Rogerio Alecrim <rogerio.alecrim@outlook.com>
2018-06-04 12:52:59 +0100

    Initial scaffolding

---
+++ MVP/MVP/Checkout/Checkout.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MVP.Services;
+using MVP.Models;
+using MVP.Models.Entities;
+
+namespace MVP.Checkout
+{
+    public partial class Checkout : System.Web.UI.Page
+    {
+        private readonly CheckoutService service = new CheckoutService();
+
+        CheckoutDTO pageData;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            InitData();
+        }
+
+        private void InitData()
+        {
+            pageData = null;
+            if (IsPostBack)
+            {
+                pageData = (CheckoutDTO)Session["checkout.data"];
+            }
+
+            if (pageData == null)
+            {
+                pageData = service.GetInitialData();
+                Session["checkout.data"] = pageData;
+            }
+        }
+    }
+}
---
+++ MVP/MVP/Checkout/Services/CheckoutService.cs
@@ -0,0 +1,20 @@
+﻿using MVP.Models;
+using MVP.Models.Entities;
+using MVP.Checkout;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MVP.Services
+{
+    public class CheckoutService
+    {
+        public CheckoutDTO GetInitialData()
+        {
+            var result = new CheckoutDTO();
+
+            return result;
+        }
+    }
+}
