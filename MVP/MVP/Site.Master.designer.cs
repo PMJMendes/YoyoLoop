de3c450d2d5cb15d6778259887363c9a54813ae2
Rogerio Alecrim <rogerio.alecrim@outlook.com>
2018-06-19 21:26:49 +0100

    Fixed checkout stripe-js form.submit form.id

--- MVP/MVP/Site.Master.designer.cs
+++ MVP/MVP/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace MVP {
     public partial class SiteMaster {
         
         /// <summary>
+        /// MasterForm control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm MasterForm;
+        
+        /// <summary>
         /// login control.
         /// </summary>
         /// <remarks>
