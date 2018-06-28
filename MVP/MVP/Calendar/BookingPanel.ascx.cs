1f3b9cc6e620d5b2749dde913b06a8f6a78282b7
Rogerio Alecrim <rogerio.alecrim@outlook.com>
2018-06-28 01:07:20 +0100

    Booking panel updates on dropdown changes

--- MVP/MVP/Calendar/BookingPanel.ascx.cs
+++ MVP/MVP/Calendar/BookingPanel.ascx.cs
@@ -62,6 +62,45 @@ namespace MVP.Calendar
                 EndRegionName = "REGIAO DE DESTINO",
                 EndAPName = "Paragem de destino"
             };
+            Clear_Errors();
+        }
+
+        internal void Databind(Selection source, string trigger, bool active)
+        {
+            PanelData.Seats = source.Seats;
+            PanelData.Cost = source.Price * source.Seats;
+            PanelData.StartTime = source.Date + source.Time;
+            PanelData.StartRegionName = source.Route.StartRegion.Name;
+            PanelData.EndRegionName = source.Route.EndRegion.Name;
+            PanelData.StartAPName = source.SAP.Name;
+            PanelData.EndAPName = source.DAP.Name;
+            if (active)
+            {
+                Clear_Errors();
+            }
+            else
+            {
+                switch (trigger)
+                {
+                    case "sap":
+                        LbStartAP.ForeColor = System.Drawing.Color.Red;
+                        break;
+                    case "dap":
+                        LbEndAP.ForeColor = System.Drawing.Color.Red;
+                        break;
+                    case "date":
+                        LbDate.ForeColor = System.Drawing.Color.Red;
+                        break;
+                    case "seats":
+                        LbSeats.ForeColor = System.Drawing.Color.Red;
+                        break;
+                }
+                BtnBook.BackColor = System.Drawing.Color.Gray;
+                BtnBook.ForeColor = System.Drawing.Color.White;
+                BtnBook.BorderColor = System.Drawing.Color.Black;
+                BtnBook.Enabled = false;
+                LbError.Visible = true;
+            }
         }
 
         protected void BtnBook_Click(object sender, EventArgs e)
@@ -76,6 +115,20 @@ namespace MVP.Calendar
         protected virtual void OnBookingSelected(BookingSelectedEventArgs args)
         {
             BookingSelected?.Invoke(this, args);
+        }
+
+        internal void Clear_Errors()
+        {
+            var labels = this.Controls.OfType<Label>();
+            foreach (Label l in labels)
+            {
+                l.ForeColor = System.Drawing.Color.Empty;
+            }
+            BtnBook.Enabled = true;
+            BtnBook.ForeColor = System.Drawing.Color.Empty;
+            BtnBook.BackColor = System.Drawing.Color.Empty;
+            BtnBook.BorderColor = System.Drawing.Color.Empty;
+            LbError.Visible = false;
         }
     }
 }
