6ba4a69e4f4bb3e63aa1da0c8979d063ab0137cf
Rogerio Alecrim <rogerio.alecrim@outlook.com>
2018-07-06 14:04:54 +0100

    Blocks on all ajax call, some bugs

--- MVP/MVP/Test.aspx.designer.cs
+++ MVP/MVP/Test.aspx.designer.cs
@@ -22,30 +22,21 @@ namespace MVP {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// DropdownMenuButton1 control.
+        /// panel control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::MVP.Controls.DropdownMenuButton DropdownMenuButton1;
+        protected global::System.Web.UI.UpdatePanel panel;
         
         /// <summary>
-        /// DropdownMenuButton2 control.
+        /// btnTest control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::MVP.Controls.DropdownMenuButton DropdownMenuButton2;
-        
-        /// <summary>
-        /// CalendarTable control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::MVP.Calendar.CalendarTable CalendarTable;
+        protected global::System.Web.UI.WebControls.Button btnTest;
     }
 }
