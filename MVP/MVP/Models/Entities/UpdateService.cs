a771819f2a11a6fda76669804ebfcfa58e94ac59
Ralek <rogerio.alecrim@outlook.com>
2018-07-20 14:41:34 +0100

    Model UpdateService

---
+++ MVP/MVP/Models/Entities/UpdateService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVP.Models.Entities
+{
+    public class UpdateService
+    {
+        public Guid UpdateServiceId { get; set; }
+        public DateTime LastRun { get; set; }
+        public DateTime LastDaily { get; set; }
+        public TimeSpan WarningThreshold { get; set; }
+    }
+}
