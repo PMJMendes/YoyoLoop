ec98f84365a5e4c10a2077ab19ff78f2abc55296
Ralek <rogerio.alecrim@outlook.com>
2018-07-30 17:06:48 +0100

    CSS tests

--- MVP/MVP/Scripts/Custom/blockUI-extension.js
+++ MVP/MVP/Scripts/Custom/blockUI-extension.js
@@ -1,17 +1,23 @@
 ﻿function pageLoad() {
     $.blockUI.defaults.message = $('#divBlockUI');
     $.blockUI.defaults.baseZ = 2000;
+    $.blockUI.defaults.css = {
+        padding: 0,
+        margin: 0,
+        width: '30%',
+        top: '40%',
+        left: '35%',
+        textAlign: 'center',
+        cursor: 'wait'
+    }
+    $.blockUI.defaults.overlayCSS = {
+        backgroundColor: '#000',
+        opacity: 0.3,
+        cursor: 'wait'
+    }
 
     if (!Sys.WebForms.PageRequestManager.getInstance().get_isInAsyncPostBack()) {
         Sys.WebForms.PageRequestManager.getInstance().add_endRequest(AjaxEnd);
         Sys.WebForms.PageRequestManager.getInstance().add_initializeRequest(AjaxBegin);
     }
-}
-
-function AjaxEnd(sender, args) {
-    $.unblockUI();
-}
-
-function AjaxBegin(sender, args) {
-    $.blockUI();
 }
