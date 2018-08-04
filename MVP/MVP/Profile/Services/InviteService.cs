9466860c7f2fea4b424cbcfdbc2da141e97d8558
Ralek <rogerio.alecrim@outlook.com>
2018-08-04 02:08:12 +0100

    profile invite

---
+++ MVP/MVP/Profile/Services/InviteService.cs
@@ -0,0 +1,27 @@
+﻿using MVP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVP.Profile.Services
+{
+    public class InviteService
+    {
+        public InviteDTO GetInitialData(string userid)
+        {
+            using (var model = new EntityModel())
+            {
+                var user = model.Users.FirstOrDefault(u => u.Id == userid);
+                var result = new InviteDTO
+                {
+                    UserId = user.Id,
+                    AmountSaved = "0",
+                    MGMTrips = "0",
+                    MGMCode = "#mypromocode"
+                };
+                return result;
+            }
+        }
+    }
+}
