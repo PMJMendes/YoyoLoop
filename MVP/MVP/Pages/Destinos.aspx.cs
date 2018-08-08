34a7cf85626c621e5259d999a6b27505914f7f14
Ralek <rogerio.alecrim@outlook.com>
2018-08-08 04:15:37 +0100

    Individual destinations and linking

--- MVP/MVP/Pages/Destinos.aspx.cs
+++ MVP/MVP/Pages/Destinos.aspx.cs
@@ -9,9 +9,50 @@ namespace MVP.Pages
 {
     public partial class Destinos : System.Web.UI.Page
     {
+        public class PageState
+        {
+            public string Destino;
+        }
+
+        protected PageState localData;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            localData = null;
 
+            if (IsPostBack)
+            {
+                localData = (PageState)Session["destinos.data"];
+            }
+
+            if (localData == null)
+            {
+                localData = new PageState();
+                var query = Request.QueryString;
+
+                if (!string.IsNullOrEmpty(query["Destino"]))
+                {
+                    switch(query["Destino"])
+                    {
+                        case "Lisboa":
+                            localData.Destino = "lisboa";
+                            break;
+                        case "Porto":
+                            localData.Destino = "porto";
+                            break;
+                        case "Cascais":
+                            localData.Destino = "cascais";
+                            break;
+                        case "Coimbra":
+                            localData.Destino = "coimbra";
+                            break;
+                        default:
+                            localData.Destino = string.Empty;
+                            break;
+                    }
+                }
+                Session["destinos.data"] = localData;
+            }
         }
     }
 }
