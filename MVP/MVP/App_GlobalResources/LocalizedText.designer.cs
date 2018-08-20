2b8fa9cce95e4546a7f87f0201d2a5e1c876a58d
Ralek <rogerio.alecrim@outlook.com>
2018-08-20 16:00:47 +0100

    StaticPages Privacy

--- MVP/MVP/App_GlobalResources/LocalizedText.designer.cs
+++ MVP/MVP/App_GlobalResources/LocalizedText.designer.cs
@@ -5380,6 +5380,141 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to 8. Nos termos legais, o utilizador tem, a todo o momento, direito à informação gratuita sobre os dados armazenados e eventualmente a correcção, bloqueio ou eliminação desses dados. Os pedidos de informação, correcção ou eliminação devem ser enviados por e-mail para clientes@goodlife.pt ou para goodlife! - Circunland S.A., - Estrada de Outurela, 118 - Parque Holanda - Edifício Holanda - 2790-114 Carnaxide..
+        /// </summary>
+        internal static string StaticPages_Privacy_Eight {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_Eight", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 5. A goodlife! transmitirá quaisquer dados pessoais a terceiros caso isso seja necessário para cumprir regulamentos legais (por exemplo, da lei de proteção de dados) como no caso da transmissão de dados a autoridades de perseguição penal e inspecção para fins de defesa de perigos para a segurança pública..
+        /// </summary>
+        internal static string StaticPages_Privacy_Five {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_Five", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 4. O nosso website utiliza uma tecnologia chamada cookies. Para facilitar e personalizar a experiência do Utilizador final com o site, a goodlife! armazena cookies no seu computador. Os Cookies são pequenos arquivos de texto que são armazenados no computador do utilizador. A maioria dos Cookies utilizados no nosso site serão apagados do disco rígido do utilizador automaticamente ao encerrar a sessão do navegador (chamados Cookies de sessão) . Outros tipos de Cookies permanecerão no computador e possibilitam [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string StaticPages_Privacy_Four {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_Four", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to O www.yoyoloop.com é um serviço da empresa Circunland S.A., doravante designada por goodlife! entidade com sede na Estrada de Outurela, 118, Parque Holanda - Edifício Holanda - 2790-114 Carnaxide. A presente política de privacidade esclarece os utilizadores do site sobre as nossas práticas de protecção da respectiva privacidade e dados pessoais e reitera as autorizações que nos foram conferidas, no que respeita a recolha, utilização e processamento de dados pessoais, no momento de registo no site enquanto u [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string StaticPages_Privacy_Intro {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_Intro", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Política de Privacidade.
+        /// </summary>
+        internal static string StaticPages_Privacy_MainTitle {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_MainTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 1. A goodlife! está autorizada a recolher dados pessoais. São considerados dados pessoais do utilizador os dados relativos ao nome, endereço electrónico, morada. O responsável pelo tratamento dos dados é a Circunland S.A. e a finalidade do tratamento reside na gestão de clientes e marketing..
+        /// </summary>
+        internal static string StaticPages_Privacy_One {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_One", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 7. Armazenamos o texto do contrato e enviamos os dados da encomenda bem como as nossas condições gerais de venda por e-mail. As condições gerais de venda podem ser consultadas online no site www.goodlife.com.pt. As suas encomendas antigas podem ser consultadas na nossa área de login de cliente..
+        /// </summary>
+        internal static string StaticPages_Privacy_Seven {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_Seven", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 6. Salvo oposição expressa aquando do registo ou em momento posterior, a goodlife! está autorizada a utilizar e processar os dados pessoais do utilizador para fins de acções de marketing (e-mail, newsletter, etc.)..
+        /// </summary>
+        internal static string StaticPages_Privacy_Six {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_Six", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to A inclusão nestes programas de publicidade pode ser cancelada a qualquer momento, por exemplo através de um e-mail para o endereço clientes@goodlife.pt. O cancelamento é possível em qualquer momento através de um link no e-mail..
+        /// </summary>
+        internal static string StaticPages_Privacy_Six_A {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_Six_A", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to A Yoyoloop! reconhece a importância de proteger os seus dados pessoais, qualquer que seja a sua natureza. Por esse motivo desenvolvemos um conjunto de medidas de segurança que visam a protecção dos seus dados..
+        /// </summary>
+        internal static string StaticPages_Privacy_SubTitle {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_SubTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 3. A goodlife! utiliza alguns sistemas de tracking de terceiros, como por exemplo o Google Analytics, um serviço de análise de internet da Google Inc. O Google Analytics utiliza Cookies (arquivos de texto) que são armazenados no computador do utilizador e possibilitam posteriormente a análise de utilização do site. As informações geradas a partir dos Cookies a respeito da utilização do site (incluindo o endereço IP) serão transmitidos a um servidor nos Estados Unidos e lá armazenados..
+        /// </summary>
+        internal static string StaticPages_Privacy_Three {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_Three", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to O Google utilizará essas informações para analisar a utilização do site e para fornecer relatórios sobre a utilização aos operadores de sites, bem como o fornecimento de outros serviços ligados à utilização do site e da internet. O Google transmitirá também essas informações a terceiros, caso seja imposto por lei ou no caso em que o terceiro processe esses dados por ordem do Google. O Google não ligará seu endereço IP a outros dados..
+        /// </summary>
+        internal static string StaticPages_Privacy_Three_A {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_Three_A", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to O utilizador pode impedir a instalação de Cookies configurando o seu navegador. Avisamos que neste caso a totalidade das funções do site não será disponível..
+        /// </summary>
+        internal static string StaticPages_Privacy_Three_B {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_Three_B", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Ao utilizar o site o utilizador concorda com o processamento dos dados levantados pelo Google na forma e na finalidade anteriormente descrita..
+        /// </summary>
+        internal static string StaticPages_Privacy_Three_C {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_Three_C", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to 2. Processamos e utilizamos os dados pessoais do utilizador para fins de realização e execução do contrato de utilização e dos contratos de compra com a goodlife! Isto abrange a transmissão dos dados pessoais a empresas de serviços ou empresas de &quot;outsourcing&quot; que contribuam para melhorar ou facilitar as operações através da goodlife! , como (sem se limitar a) meios de pagamentos, seguros ou intermediários na gestão de pagamentos e outros. Estas empresas ou sites de internet têm no geral politicas de privac [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string StaticPages_Privacy_Two {
+            get {
+                return ResourceManager.GetString("StaticPages_Privacy_Two", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to The card was declined..
         /// </summary>
         internal static string Stripe_ErrorHandling_CardError_card_declined {
