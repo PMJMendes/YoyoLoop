cb64ac249c526f47c4fe30e72f434efb8775add8
Ralek <rogerio.alecrim@outlook.com>
2018-12-01 15:50:43 +0000

    ticket email text

--- MVP/MVP/App_GlobalResources/LocalizedText.designer.cs
+++ MVP/MVP/App_GlobalResources/LocalizedText.designer.cs
@@ -880,6 +880,15 @@ namespace Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Para visualizar o local no mapa clique no nome da paragem.
+        /// </summary>
+        internal static string Confirm_Service_SendTicket_Email_Body_SeeOnMap {
+            get {
+                return ResourceManager.GetString("Confirm_Service_SendTicket_Email_Body_SeeOnMap", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Obrigado por escolheres a Yoyoloop!&lt;br&gt;Se necessário ligue-nos para o +351 926 475 250&lt;br&gt;.
         /// </summary>
         internal static string Confirm_Service_SendTicket_Email_Body_ThankYou {
